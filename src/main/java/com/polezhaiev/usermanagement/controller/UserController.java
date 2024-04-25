a6707e82c5af52f23a799a31dd17f2c0d4606d10
Andrey Polezhaiev <workpolezhaiev@gmail.com>
2024-04-25 17:30:45 +0300

    add user controller

--- src/main/java/com/polezhaiev/usermanagement/controller/UserController.java
+++ src/main/java/com/polezhaiev/usermanagement/controller/UserController.java
@@ -7,8 +7,8 @@ import com.polezhaiev.usermanagement.dto.UserResponseDto;
 import com.polezhaiev.usermanagement.service.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,13 +26,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
-    @Operation(summary = "Register a new user", description = "Register a new user")
+    @Operation(summary = "Register a new user",
+            description = "Register a new user")
     @PostMapping
     public UserResponseDto register(@RequestBody @Valid CreateUserRequestDto requestDto) {
         return userService.createUser(requestDto);
     }
 
-    @Operation(summary = "Update user's phone number", description = "Update user's phone number")
+    @Operation(summary = "Update user's phone number",
+            description = "Update user's phone number")
     @PutMapping("/phone/{id}")
     public UserResponseDto updatePhoneNumber(
             @PathVariable Long id,
@@ -40,7 +42,8 @@ public class UserController {
         return userService.updatePhoneNumber(id, requestDto);
     }
 
-    @Operation(summary = "Update the user", description = "Update the user")
+    @Operation(summary = "Update the user",
+            description = "Update the user")
     @PutMapping("/{id}")
     public UserResponseDto updateUser(
             @PathVariable Long id,
@@ -48,13 +51,15 @@ public class UserController {
         return userService.updateUser(id, requestDto);
     }
 
-    @Operation(summary = "Delete the user by id", description = "Delete the user by id")
+    @Operation(summary = "Delete the user by id",
+            description = "Delete the user by id")
     @DeleteMapping("/{id}")
     public boolean deleteById(@PathVariable Long id) {
         return userService.deleteById(id);
     }
 
-    @Operation(summary = "Search users by birthdate range", description = "Search users by birthdate range")
+    @Operation(summary = "Search users by birthdate range",
+            description = "Search users by birthdate range")
     @GetMapping
     public List<UserResponseDto> searchUsersByBirthDateRange(
             @RequestBody UserBirthDateRangeRequestDto requestDto) {
--- src/main/java/com/polezhaiev/usermanagement/dto/UserBirthDateRangeRequestDto.java
+++ src/main/java/com/polezhaiev/usermanagement/dto/UserBirthDateRangeRequestDto.java
@@ -1,8 +1,7 @@
 package com.polezhaiev.usermanagement.dto;
 
-import lombok.Data;
-
 import java.time.LocalDateTime;
+import lombok.Data;
 
 @Data
 public class UserBirthDateRangeRequestDto {
